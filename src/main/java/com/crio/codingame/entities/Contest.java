ebf095e42ee239475c9b56949d7d016657b93089
CrioUser <crio-user@criodo.com>
2022-10-09 19:23:09 +0530

    ME_OBJECT_MODELING_V2_MODULE_SERVICES

--- src/main/java/com/crio/codingame/entities/Contest.java
+++ src/main/java/com/crio/codingame/entities/Contest.java
@@ -27,7 +27,8 @@ public class Contest extends BaseEntity{
     public Contest(String name, List<Question> questions, Level level, User creator,
             ContestStatus contestStatus) {
         this.name = name;
-        this.questions = new ArrayList<>();
+        // this.questions = new ArrayList<>();
+        this.questions = questions;
         validateQuestionList(questions, level);
         this.level = level;
         this.creator = creator;
